049ec46bcc918f74fd49fded03ca8dc2bcee5d38
Guilherme Ventura sousa <61128802+GuilhermeVenturaSousa@users.noreply.github.com>
2020-04-02 20:26:21 -0300

    criacao do escopo do programa

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,37 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Enter account data ");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit =sc.nextDouble();
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		acc.withdraw(sc.nextDouble());
+		System.out.println("New balance: " + acc.getBalance());
+		
+		
+		
+		sc.close();
 	}
 
 }
